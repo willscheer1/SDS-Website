3c36ca7259295f3b968be8c2ee4d217e30c82699
-- <-->
2024-02-05 18:13:19 -0600

    Completed Video page

--- scripts/scripts_videos.js
+++ scripts/scripts_videos.js
@@ -1,4 +1,5 @@
 
+// Set background color of selector buttons
 function selectBGColor(target) {
     let selectors = document.getElementsByClassName("selector");
     let selectorsArray = Array.from(selectors);
@@ -11,3 +12,148 @@ function selectBGColor(target) {
         }   
     });
 }
+
+// Param: path to background image / video file
+function createVideoThumbnail(thumbnail, video) {
+    const container = document.createElement("div");
+    container.classList.add("media-container");
+    container.classList.add("video-container");
+    container.style.backgroundImage = "url(" + thumbnail + ")";
+
+    const playButton = document.createElement("img");
+    playButton.src = "images/play-button.svg";
+    playButton.classList.add("play-button");
+    container.appendChild(playButton);
+
+    document.getElementById("video-grid").appendChild(container);
+}
+
+// Param = "All", "Event", "Promotional", or "Cinema"
+// Returns dictionary with video:thumbnail key/value pairs
+function getVideos(type) {
+    const videoPath = "video/portfolio-videos/";
+    const thumbnailPath = "images/video-thumbnails/"
+    const thumbnailPrefix = "THUMBNAIL-";
+    const videoFileType = ".mp4";
+    const imgFileType = ".jpg";
+
+    result = new Map();
+
+    // Get event videos/thumbnails
+    function getEvents() {
+        const folder = "Event/";
+        const videoPrefix = "EVENT_";
+
+        const eventVideos = [
+            "balloons",
+            "concert",
+            "crowd",
+            "fireworks",
+        ];
+
+        eventVideos.forEach(function(title) {
+            result.set(videoPath + folder + videoPrefix + title + videoFileType,
+                thumbnailPath + folder + thumbnailPrefix + videoPrefix + title + imgFileType);
+        });
+    }
+
+    // Get promotional videos/thumbnails
+    function getPromos() {
+        const folder = "Promotional/";
+        const videoPrefix = "PROMO_";
+
+        const promoVideos = [
+            "district",
+            "glass-building",
+            "ship",
+            "sky-scraper"
+        ];
+
+        promoVideos.forEach(function(title) {
+            result.set(videoPath + folder + videoPrefix + title + videoFileType,
+                thumbnailPath + folder + thumbnailPrefix + videoPrefix + title + imgFileType);;
+        });
+    }
+
+    // Get cinematic videos/thumbnails
+    function getCinemas() {
+        const folder = "Cinematic/";
+        const videoPrefix = "CINEMA_";
+
+        const cinemaVideos = [
+            "dirt-bike",
+            "forest-road",
+            "mountain",
+            "snowscape"
+        ];
+
+        cinemaVideos.forEach(function(title) {
+            result.set(videoPath + folder + videoPrefix + title + videoFileType,
+                thumbnailPath + folder + thumbnailPrefix + videoPrefix + title + imgFileType);;
+        });
+    }
+
+    // Get videos based on type selected
+    switch (type) {
+        case "All":
+            getEvents();
+            getPromos();
+            getCinemas();
+            break;
+        case "Event":
+            getEvents();
+            break;
+        case "Promotional":
+            getPromos();
+            break;
+        case "Cinema":
+            getCinemas();
+            break;
+    }
+    
+    return result;
+}
+
+// Clear contents of a container
+function clearContainer(id) {
+    container = document.getElementById(id);
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+// Set dropdown selected
+function setDropdown(target) {
+    const dropdown = document.getElementById("selector-dropdown");
+    switch (target.textContent) {
+        case "All":
+            dropdown.options.selectedIndex = 0;
+            break;
+        case "Event":
+            dropdown.options.selectedIndex = 1;
+            break;
+        case "Promotional":
+            dropdown.options.selectedIndex = 2;
+            break;
+        case "Cinematic":
+            dropdown.options.selectedIndex = 3;
+            break;
+    }
+}
+
+// Display videos based on selector
+function select(target) {
+    clearContainer("video-grid");
+    videos = getVideos(target.textContent);
+    videos.forEach(function(video, thumbnail) {
+        createVideoThumbnail(video, thumbnail);
+    })
+    setDropdown(target);
+}
+
+// Default load all
+document.addEventListener("DOMContentLoaded", function() {
+    select(document.getElementById("selector-default"));
+});
+
